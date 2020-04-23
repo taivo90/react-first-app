15115fe93275dc9578b94e00b337bceb159c3629
Tai <tai@Viets-MacAir-2.local>
2020-04-23 15:20:52 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Spinner } from 'reactstrap';
+import Material from './Material'
+import Card from './Card'
+
+
+var moment = require('moment');
+
+
+const collections = [
+  {title: "A", author:"Nguyen A"},
+  {title :"B", author :"Nguyen B"},
+  {title :"C", author :"Nguyen C"},
+]
 
 function App() {
+
+  const str = moment.duration(1, "minutes").humanize(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {collections.map( collection =>{
+          return (
+          <li>{collection.title} - {collection.author}</li>
+          )
+        })}
+      </ul>
+      <Material />
+      <Card />
+      <div>
+      <Button color="primary">primary</Button>{' '}
+      <Button color="secondary">secondary</Button>{' '}
+      <Button color="success">success</Button>{' '}
+      <Button color="info">info</Button>{' '}
+      <Button color="warning">warning</Button>{' '}
+      <Button color="danger">danger</Button>{' '}
+      <Button color="link">link</Button>
+
+      <div>
+      <Spinner color="primary" />
+      <Spinner color="secondary" />
+      <Spinner color="success" />
+      <Spinner color="danger" />
+      <Spinner color="warning" />
+      <Spinner color="info" />
+      <Spinner color="light" />
+      <Spinner color="dark" />
+    </div>
+    </div>
     </div>
   );
 }
